bf4424edf651a86e4505b461628bc574853a6120
LucyYYW <yueweilucy@126.com>
2019-09-06 02:29:12 -0400

    setting up login/signup view controllers

---
+++ FortuneU/View Controllers/SignupViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignupViewController.h
+//  FortuneU
+//
+//  Created by User on 9/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignupViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
