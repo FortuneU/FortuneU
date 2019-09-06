fb69d2f3233d7d4f7b8174bc882ce0817b968c82
LucyYYW <yueweilucy@126.com>
2019-09-06 02:07:56 -0400

    real initial commit

---
+++ FortuneU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FortuneU
+//
+//  Created by User on 9/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FortuneU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FortuneU
+//
+//  Created by User on 9/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
