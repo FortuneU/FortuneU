60862a14f7c2962ed8391b0e5a2bdf9b1be6b451
LucyYYW <yueweilucy@126.com>
2019-09-06 18:46:38 -0400

    added a bunch of view controllers files for later prep

---
+++ FortuneU/View Controllers/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  FortuneU
+//
+//  Created by User on 9/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FortuneU/View Controllers/TargetViewController.h
@@ -0,0 +1,17 @@
+//
+//  TargetViewController.h
+//  FortuneU
+//
+//  Created by User on 9/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TargetViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
