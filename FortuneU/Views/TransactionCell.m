87b134cd4b7e4f431d9e243bcabdbe6cf1a1107b
LucyYYW <yueweilucy@126.com>
2019-09-07 09:18:58 -0400

    setup record page look + add transaction cell file and xib

---
+++ FortuneU/Views/TransactionCell.m
@@ -0,0 +1,24 @@
+//
+//  TransactionCell.m
+//  FortuneU
+//
+//  Created by User on 9/7/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+#import "TransactionCell.h"
+
+@implementation TransactionCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
